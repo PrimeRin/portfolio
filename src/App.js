cbbd1efc2be3fb8845e647e37ea777e0f4185c9b
PrimeRin <primedruk312@gmail.com>
2024-06-04 16:26:59 +0600

    testimonial complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Services from "./components/Services/Services";
 import Experience from "./components/Experience/Experience";
 import Work from "./components/Work/Work";
 import Portfolio from "./components/Portfolio/Portfolio";
+import Testimonials from "./components/Testimonials/Testimonials";
 import './App.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Experience/>
       <Work/>
       <Portfolio/>
+      <Testimonials/>
     </div>
   );
 }
